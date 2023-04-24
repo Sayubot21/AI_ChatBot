2a523122f2b8aa82874b32645539d17aa8824974
SAYOOJ P V <90135690+Sayoojpv4@users.noreply.github.com>
2023-04-24 17:19:35 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 
 const openai = new OpenAIApi(configuration);
